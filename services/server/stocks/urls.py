a27876297cab8e882da125fefe7ec8301e079f7d
jamedeo <joeamedeo@gmail.com>
2020-04-19 00:16:31 -0500

    Server up on webfaction.  Let's gooooooo!!!!!!

--- services/server/stocks/urls.py
+++ services/server/stocks/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from stocks import views
 
 urlpatterns = [
     path('symbol/', views.symbol_list),
-    path('symbol/<int:pk>/', views.symbol_detail),
+    re_path(r'analysis$', views.symbol_detail),
 ]
--- services/server/stocks/views.py
+++ services/server/stocks/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 # DRF
-from rest_framework.parsers import JSONParser
+# from rest_framework.parsers import JSONParser
 
 # Create your views here.
 from .models import NYSEMin, NYSEDay, NASDAQMin, NASDAQDay, IndexesDay, IndexesMin, Fundamental
@@ -25,14 +25,14 @@ def symbol_list(request):
 			datetime__gte = datetime.date(2019, 10, 1),
 			datetime__lt = datetime.date(2020, 4, 1)
 			).order_by('symbol', 'datetime_epoch'
-			).distinct('symbol', 'open', 'high', 'close', 'volume', 'datetime_epoch', 'datetime'
+			).distinct('symbol', 'open', 'high','low', 'close', 'volume', 'datetime_epoch', 'datetime'
 			)
 
 		_dict = {
 			'symbol'		 : [row.symbol for row in symbols],
 			'open'  		 : [row.open for row in symbols],
 			'high'  		 : [row.high for row in symbols],
-			'low'  		 : [row.low for row in symbols],
+			'low'  		 	 : [row.low for row in symbols],
 			'close' 		 : [row.close for row in symbols],
 			'volume' 		 : [row.volume for row in symbols],
 			'datetime_epoch' : [row.datetime_epoch for row in symbols],
@@ -43,30 +43,42 @@ def symbol_list(request):
 			content_type = 'application/javascript; charset=utf8'
 			)
 
-	return JsonResponse(serializer.errors, status=400)
+	return HttpResponse('Hello World')
 
 @csrf_exempt
-def symbol_detail(request, pk):
+def symbol_detail(request):
 	"""
 	List all code snippets, or create a new snippet.
 	"""
 	if request.method == 'GET':
-		symbols = NASDAQDay.objects.filter(symbol='AAPL')
+		symbol = request.GET['symbol'].upper()
+		start_date = request.GET['start_date'].split('-')
+		start_date = [int(row) for row in start_date]
+
+		end_date = request.GET['end_date'].split('-')
+		end_date = [int(row) for row in end_date]
+		print(symbol)
+		symbols = NASDAQDay.objects.filter(
+			symbol = symbol,
+			datetime__gte = datetime.date(start_date[0], start_date[1], start_date[2]),
+			datetime__lte = datetime.date(end_date[0], end_date[1], end_date[2])
+			).order_by('symbol', 'datetime_epoch'
+			).distinct('symbol', 'open', 'high','low', 'close', 'volume', 'datetime_epoch', 'datetime'
+			)
 
 		_dict = {
 			'symbol'		 : [row.symbol for row in symbols],
 			'open'  		 : [row.open for row in symbols],
 			'high'  		 : [row.high for row in symbols],
+			'low'  		 	 : [row.low for row in symbols],
 			'close' 		 : [row.close for row in symbols],
 			'volume' 		 : [row.volume for row in symbols],
 			'datetime_epoch' : [row.datetime_epoch for row in symbols],
-			# 'datetime' 		 : [row.datetime for row in symbols],
+			'datetime' 		 : [row.datetime.isoformat() for row in symbols],
 		}
-		print(json.dumps(_dict))
-		# print(_dict['open'])
 		return HttpResponse(
 			json.dumps(_dict),
 			content_type = 'application/javascript; charset=utf8'
 			)
 
-	return JsonResponse(serializer.errors, status=400)
+	return HttpResponse('Hello World')
