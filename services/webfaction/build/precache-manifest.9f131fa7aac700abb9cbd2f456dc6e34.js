a9b7e031d15ff51b7ea122175d334e3ddde5d253
Arun George <arunbgeo@gmail.com>
2020-04-19 23:58:41 -0400

    update builds to copy into webfaction server as well

---
+++ services/webfaction/build/precache-manifest.9f131fa7aac700abb9cbd2f456dc6e34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af892c509776c965fcc0efb5066d8e92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82846f8bb7ed7aa5ad6b",
+    "url": "./static/css/main.919d1569.chunk.css"
+  },
+  {
+    "revision": "d9ccaeb5340ff4c8bd3d",
+    "url": "./static/js/2.5335fe8f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.5335fe8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82846f8bb7ed7aa5ad6b",
+    "url": "./static/js/main.bd72cef5.chunk.js"
+  },
+  {
+    "revision": "1e8c7afc7bf36ad0e5f1",
+    "url": "./static/js/runtime-main.64664b1b.js"
+  },
+  {
+    "revision": "456ede4eda7fbc6fb37bd64b800ab439",
+    "url": "./static/media/stocks.456ede4e.jpg"
+  }
+]);
